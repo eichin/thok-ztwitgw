de6e30bcd2411736f36a57ade3a58876ce1b0e05
Mark Eichin <eichin@thok.org>
2011-08-06 22:59:17 -0400

    based on   http://docs.python.org/distutils/introduction.html   http://docs.python.org/distutils/setupscript.html Annoyed that the packaging "version" is redundant with __version__ in the module, though

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='ztwit',
+      version='0.2',
+      py_modules=['ztwitgw', 'zpost'],
+      description='Zephyr/Twitter Gateway Tools',
+      author='Mark Eichin',
+      author_email='eichin@thok.org',
+      url='http://thok.org/intranet/python/ztwit/README.html',
+      )
