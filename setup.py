7c7c6a054bd7102e8210cf3babc7569d7ac55f20
Mark Eichin <eichin@thok.org>
2020-09-29 03:32:31 -0400

    long-missing deps

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from distutils.core import setup
 setup(name='ztwit',
       version='0.4',
       py_modules=['ztwitgw', 'zpost', 'lengthener'],
+      install_requires=[
+          "tweepy",
+          "oauth",
+      ],
       description='Zephyr/Twitter Gateway Tools',
       author='Mark Eichin',
       author_email='eichin@thok.org',
